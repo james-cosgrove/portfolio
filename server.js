ab0d59e3aedfc9d19d7b767bb500ea8d0b6abd8a
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-02-28 22:34:04 +1100

    remove fixed port number in server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var server = express();
 
 // const PORT = 3000;
-var portNumber = server.listen(process.env.PORT || 3000);
+var portNumber = server.listen(process.env.PORT);
 
 server.use(express.static(path.join(__dirname, './')));
 
