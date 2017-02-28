fd469b48eee958a5e3a4a8ac6772c3b099f15729
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-02-28 23:24:19 +1100

    fix error with server file

--- app/server.js
+++ app/server.js
@@ -3,16 +3,16 @@ var path = require('path');
 var server = express();
 
 // const PORT = 3000;
-var portNumber = server.listen(process.env.PORT);
+var portNumber = process.env.PORT || 3000;
 
-// server.use(express.static(path.join(__dirname, './')));
+server.use(express.static(path.join(__dirname, './')));
 
 server.get('/', function(request, response) {
   response.render('index');
 });
 
-server.listen(PORT, function() {
-  console.log(`Example app listening on port ${PORT}`);
+server.listen(portNumber, function() {
+  console.log(`App listening on port ${PORT}`);
 });
 
-server.listen(portNumber);
+// server.listen(portNumber);
