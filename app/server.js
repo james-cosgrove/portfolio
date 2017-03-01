eb4ba76551a33f11b3b890eb92ab88f796c0ff33
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-03-01 14:11:28 +1100

    add view engine

--- app/server.js
+++ app/server.js
@@ -6,6 +6,8 @@ var server = express();
 var portNumber = process.env.PORT || 3000;
 
 // server.use(express.static(path.join(__dirname, './')));
+server.set('views', './');
+server.set('view engine', 'ejs');
 server.use('/static', express.static(path.join(__dirname, 'public')));
 
 server.get('/', function(request, response) {
