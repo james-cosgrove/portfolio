c137a232dc1c16afb96b6b5db122545eec244773
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-03-01 14:35:13 +1100

    add images to public

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var portNumber = process.env.PORT || 3000;
 // server.use(express.static(path.join(__dirname, './')));
 // server.set('views', './');
 server.set('view engine', 'ejs');
-server.use('/static', express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', function(request, response) {
   response.render('index');
